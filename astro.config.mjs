b11e75ada6e2edf23363b90aa375e093b0a4117c
JuanJCoppede <jcoppede11@gmail.com>
2025-05-31 22:12:58 -0300

    feat: Improve SEO and accessibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react()
+  ],
+  output: 'static',
 });
