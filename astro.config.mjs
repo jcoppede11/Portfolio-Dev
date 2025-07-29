3d962606a804ac04a49ec731fc18cb4fb38d7dc1
JuanJCoppede <jcoppede11@gmail.com>
2025-07-29 11:53:02 -0300

    fix(deps): update brace-expansion due to security advisory

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [
-    tailwind(),
     react()
   ],
+
   output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ src/constants/sectionIds.ts
@@ -0,0 +1,8 @@
+export const SECTION_IDS = {
+    aboutMe: "jjc-about-me",
+    experience: "jjc-experience",
+    technology: "jjc-technology",
+    createdProjects: "jjc-projects-created",
+    inProgressProjects: "jjc-projects-in-progress",
+    contact: "jjc-contact",
+};
